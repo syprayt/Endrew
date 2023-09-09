78137b6702522786fe0702c4c92585a832976235
syprayt <sypersyprayt@gmail.com>
2023-09-09 11:56:11 +0300

    Підключення бібліотеків

--- Main.py
+++ Main.py
@@ -1,6 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
-
+#
 response = requests.get("https://sinoptik.ua/")
 if response.status_code == 200:
     bs = BeautifulSoup(response.text, features="html.parser")
