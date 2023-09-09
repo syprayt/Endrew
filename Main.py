0dc05b59eca452f05f922a1562692ad3a1cbde84
syprayt <sypersyprayt@gmail.com>
2023-09-09 11:55:04 +0300

    Підключення бібліотеків

--- Main.py
+++ Main.py
@@ -1,5 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
 
-response = requests.get("https://www.pokemon.com/us")
-print(response.text)
+response = requests.get("https://sinoptik.ua/")
+if response.status_code == 200:
+    bs = BeautifulSoup(response.text, features="html.parser")
+    list = bs.find("p", {"class":"today-temp"})
+    for elem in list:
+        print(elem.text)
---
+++ Main2.py
@@ -0,0 +1,9 @@
+import sqlite3
+connection = sqlite3.connect("itstep_database.sl3", 5)
+cur = connection.cursor()
+#cur.execute("CREATE TABLE first_table (name TEXT);")
+#cur.execute("INSERT INTO first_table (name, age) VALUES ('Andrey', '22');")
+#cur.execute("DELETE first_table (name) VALUES ('Age');")
+#cur.execute("SELECT INTO first_table (name) VALUES ('Nick');")
+connection.commit()
+connection.close()
