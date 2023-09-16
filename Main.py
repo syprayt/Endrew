89cfa23bbb6ff31339c7286d01cba9d8f5925af8
syprayt <sypersyprayt@gmail.com>
2023-09-16 10:33:55 +0300

    Урок11

--- Main.py
+++
@@ -1,9 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-#
-response = requests.get("https://sinoptik.ua/")
-if response.status_code == 200:
-    bs = BeautifulSoup(response.text, features="html.parser")
-    list = bs.find("p", {"class":"today-temp"})
-    for elem in list:
-        print(elem.text)
